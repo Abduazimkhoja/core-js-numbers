886c18a760704fb387f2106230012c21365afc9a
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-12-25 22:17:46 +0500

    refactor: toPrecision

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -372,9 +372,10 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
-}
+const toPrecision = (number, precision) =>
+  `${number}`.length > precision
+    ? number.toFixed(precision)
+    : `${number}`.slice(0, precision);
 
 /**
  * Returns the primitive value of a Number object.
