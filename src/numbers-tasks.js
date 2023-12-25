05ad1ddf92ac0ffd3c8924e4659bd01266bbc2ec
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-12-25 15:52:27 +0500

    refactor: get max number

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -557,8 +557,8 @@ function getSumOfNumbers(/* x1, x2, x3 */) {
  * -5, -6 => -5
  * 0, 5   => 5
  */
-function getMaxNumber(/* firstNumber, secondNumber */) {
-  throw new Error('Not implemented');
+function getMaxNumber(f, s) {
+  return f > s ? f : s;
 }
 
 /**
